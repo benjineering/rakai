55548a3c785998ab90f5a893dfa39429a4b1df03
Ben Williams <8enwilliams@gmail.com>
2016-12-11 22:48:39 +1100

    fixed filename whitespace

--- lib/rakai.rb
+++ lib/rakai.rb
@@ -4,9 +4,15 @@ require 'wavefile'
 
 include WaveFile
 
+
 module Rakai
   BLOCK_SIZE = 1024
+
+  def self.trim_str(str)
+    str.gsub(/\s+$/, '')
+  end
 end
+
 
 disk_file = File.open('/Users/ben/repos/rakai/spec/disk_images/S900/808.img')
 s9000_808 = Rakai::S900::Volume.read(disk_file)
@@ -16,7 +22,8 @@ disk_file.seek(entry.start_block * Rakai::BLOCK_SIZE)
 sample = Rakai::S900::Sample.read(disk_file)
 format = Format.new(:mono, :pcm_16, sample.sample_rate.to_i)
 buffer = Buffer.new(sample.pcm, format)
+file_name = Rakai.trim_str(sample.file_name)
 
-Writer.new("/Users/ben/Desktop/#{sample.file_name}.wav", format) do |writer|
+Writer.new("/Users/ben/Desktop/#{file_name}.wav", format) do |writer|
   writer.write(buffer)
 end
